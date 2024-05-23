079b0f62734fd82e11de9a766b7b2c411bfaa53a
Muhammad Faizan <40329896+mohammadfaizan410@users.noreply.github.com>
2024-05-23 16:42:42 +0300

    setting

--- app/src/main/java/com/ayeshanizam/ayeshaprojectv2/SettingPage.kt
+++ app/src/main/java/com/ayeshanizam/ayeshaprojectv2/SettingPage.kt
@@ -10,32 +10,19 @@ import com.ayeshanizam.ayeshaprojectv2.R
 import com.ayeshanizam.ayeshaprojectv2.auth.LoginActivity
 import com.ayeshanizam.ayeshaprojectv2.constants.Constants
 import com.ayeshanizam.ayeshaprojectv2.databinding.ActivitySettingPageBinding
-
+import com.ayeshanizam.ayeshaprojectv2.userdb.UserRoomDatabase
 
 
 class SettingPage : AppCompatActivity() {
     lateinit var binding: ActivitySettingPageBinding;
     lateinit var sharedpreferences: SharedPreferences
+    lateinit var userdb : UserRoomDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySettingPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        sharedpreferences = getSharedPreferences(Constants.SHARED_PREFS, Context.MODE_PRIVATE);
-        binding.SaveUserNameBtn.setOnClickListener {
-            val newUsername = binding.etUserName.text.toString()
-            val usernameSize =binding.etUserName.text.toString().length
-            if(newUsername != null && usernameSize > 5 ){
-                val editor = sharedpreferences.edit();
-                editor.putString(Constants.USERNAME_KEY, binding.etUserName.text.toString());
-                editor.apply();
-
-                Toast.makeText(this,"user name has been updated", Toast.LENGTH_LONG).show()
-            }
-            else{
-                Toast.makeText(this,"you need to enter a valid new username", Toast.LENGTH_LONG).show()
-            }
-
-        }
+        userdb = UserRoomDatabase.getDatabase(this)
+        sharedpreferences = getSharedPreferences(Constants.SHARED_PREFS, Context.MODE_PRIVATE)
 
         binding.logoutBtn.setOnClickListener {
             val editor = sharedpreferences.edit();
@@ -46,9 +33,25 @@ class SettingPage : AppCompatActivity() {
             finish()
         }
         binding.SavePasswordBtn.setOnClickListener {
-            val currentPass = binding.etCurrentPassword.text.toString()
-            val usernameSize =binding.etUserName.text.toString().length
-            val editor = sharedpreferences.edit();
+            val username = sharedpreferences.getString(Constants.USERNAME_KEY, "")
+            val user = userdb.userDao().getUser(username.toString())
+            if(binding.etCurrentPassword.text.toString() == "" || binding.etNewPassword.text.toString() == ""){
+                Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if(user.toString().isNullOrEmpty()){
+                Toast.makeText(this, "User not found", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if(user?.password != binding.etCurrentPassword.text.toString()){
+                Toast.makeText(this, "Current password is incorrect", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            user?.password = binding.etNewPassword.text.toString()
+            userdb.userDao().updateUser(user);
+            Toast.makeText(this, "Password updated", Toast.LENGTH_SHORT).show()
+            binding.etCurrentPassword.text.clear()
+            binding.etNewPassword.text.clear()
         }
 
 
@@ -82,6 +85,7 @@ class SettingPage : AppCompatActivity() {
             }
         }
     }
+
 
 
 }
